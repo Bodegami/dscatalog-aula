b98f832c53e050543b0e015a0b3c1e62ead9a0fd
Renato Ferreira Oliveira <65985509+Bodegami@users.noreply.github.com>
2021-10-27 21:03:10 -0300

    database seeding

--- backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
@@ -2,6 +2,8 @@ package com.devsuperior.dscatalog.services;
 
 import java.util.List;
 
+import javax.transaction.Transactional;
+
 import org.springframework.stereotype.Service;
 
 import com.devsuperior.dscatalog.entities.Category;
@@ -16,7 +18,7 @@ public class CategoryService {
 		this.repository = repository;
 	}
 
-
+	@Transactional()
 	public List<Category> findAll() {
 		List<Category> list = repository.findAll();
 		return list;
