5860f515793f6668bb2042a4959e475b8f73b3d9
Renato Ferreira Oliveira <65985509+Bodegami@users.noreply.github.com>
2021-10-28 23:35:43 -0300

    Update category

--- backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+import javax.persistence.EntityNotFoundException;
 import javax.transaction.Transactional;
 
 import org.springframework.stereotype.Service;
@@ -11,13 +12,13 @@ import org.springframework.stereotype.Service;
 import com.devsuperior.dscatalog.dto.CategoryDto;
 import com.devsuperior.dscatalog.entities.Category;
 import com.devsuperior.dscatalog.repositories.CategoryRepository;
-import com.devsuperior.dscatalog.services.exceptions.EntityNotFoundException;
+import com.devsuperior.dscatalog.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class CategoryService {
-	
+
 	private final CategoryRepository repository;
-	
+
 	public CategoryService(CategoryRepository repository) {
 		this.repository = repository;
 	}
@@ -27,11 +28,11 @@ public class CategoryService {
 		List<Category> list = repository.findAll();
 		return list.stream().map(cat -> new CategoryDto(cat)).collect(Collectors.toList());
 	}
-	
+
 	@Transactional()
 	public CategoryDto findById(Long id) {
 		Optional<Category> obj = repository.findById(id);
-		Category entity = obj.orElseThrow(() -> new EntityNotFoundException("Resource not found.."));
+		Category entity = obj.orElseThrow(() -> new ResourceNotFoundException("Resource not found.."));
 		return new CategoryDto(entity);
 	}
 
@@ -41,6 +42,19 @@ public class CategoryService {
 		entity.setName(dto.getName());
 		entity = repository.save(entity);
 		return new CategoryDto(entity);
+	}
+
+	@Transactional
+	public CategoryDto update(Long id, CategoryDto dto) {
+		try {
+			Category entity = repository.getOne(id);
+			entity.setName(dto.getName());
+			entity = repository.save(entity);
+			return new CategoryDto(entity);
+		} 
+		catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException("Id not found " + id);
+		}
 	}
 
 }
